a4d6733e41e3e99d8c15b5283ba7fe7b537e4a5a
Patrick Marshall <marshallpt@vcu.edu>
2021-12-20 13:47:20 -0500

    Fix None value crashing node
    Added check to ensure none of the IMU values are None before
    trying to create an Imu ROS2 msg.
    
    .gitignore
    * added .gitignore with boilerplate HIVE Lab stuff
    
    bno055_driver.py
    * added verify_values function to ensure no data contains
    None type

--- bno055_driver/bno055_driver.py
+++ bno055_driver/bno055_driver.py
@@ -5,49 +5,90 @@ import board
 import adafruit_bno055
 
 
+def verify_values(value_tuple):
+    """
+    Iterate through a tuple and return False if any values are None,
+    return True otherwise.
+
+    :param value_tuple: tuple to check for None values
+    :return: boolean
+    """
+    valid_data = True
+
+    for value in value_tuple:
+        if value is None:
+            valid_data = False
+
+    return valid_data
+
+
 class ReadIMU(Node):
     def __init__(self):
         super().__init__('imu_driver')
-        
+
         self.i2c = board.I2C()
         self.sensor = adafruit_bno055.BNO055_I2C(self.i2c)
 
         # Declaring parameters
         self.declare_parameter('imu_topic', 'imu')
-                        
+
         # Reading in parameters for node, by default the parameters can be found
         # under config/params.yaml
         self.imu_topic = self.get_parameter('imu_topic')
-                                      
+
         # Create topic publishers for IMU        
-        self.imu_output = self.create_publisher(Imu, self.imu_topic.value , 1)
-        
+        self.imu_output = self.create_publisher(Imu, self.imu_topic.value, 1)
+
         # Create timer for IMU publisher.
-        self.rate = 100 # 100 Hz
+        self.rate = 100  # 100 Hz
         self.timer_period = 1.0 / self.rate
         self.timer = self.create_timer(self.timer_period, self._publish_imu_msg)
-      
+
     def _publish_imu_msg(self):
-        
+        """
+        Read values from IMU on I2C bus. Verify these values are valid (not None).
+        Once this is confirmed, create and publish an Imu message onto imu_topic param.
+
+        :return: nothing
+        """
         imuMsg = Imu()
-        imuMsg.header.stamp =  self.get_clock().now().to_msg()
-        imuMsg.header.frame_id = 'imu'                      
-        
-        imuMsg.orientation.x = float(self.sensor.quaternion[0])
-        imuMsg.orientation.y = float(self.sensor.quaternion[1])
-        imuMsg.orientation.z = float(self.sensor.quaternion[2])
-        imuMsg.orientation.w = float(self.sensor.quaternion[3])
+        imuMsg.header.stamp = self.get_clock().now().to_msg()
+        imuMsg.header.frame_id = 'imu'
 
-        imuMsg.angular_velocity.x = float(self.sensor.gyro[0])
-        imuMsg.angular_velocity.y = float(self.sensor.gyro[1])
-        imuMsg.angular_velocity.z = float(self.sensor.gyro[2])
+        valid_values = False
 
-        imuMsg.linear_acceleration.x = float(self.sensor.linear_acceleration[0])
-        imuMsg.linear_acceleration.y = float(self.sensor.linear_acceleration[1])
-        imuMsg.linear_acceleration.z = float(self.sensor.linear_acceleration[2])
+        quaternion_values = (self.sensor.quaternion[0], self.sensor.quaternion[1], self.sensor.quaternion[2],
+                             self.sensor.quaternion[3])
+        gyro_values = (self.sensor.gyro[0], self.sensor.gyro[1], self.sensor.gyro[2])
+        linear_acceleration_values = (self.sensor.linear_acceleration[0], self.sensor.linear_acceleration[1],
+                                      self.sensor.linear_acceleration[2])
 
-        self.imu_output.publish(imuMsg)
-            
+        quaternion_integrity = verify_values(quaternion_values)
+        gyro_integrity = verify_values(gyro_values)
+        linear_acceleration_integrity = verify_values(linear_acceleration_values)
+
+        if quaternion_integrity and gyro_integrity and linear_acceleration_integrity:
+            valid_values = True
+
+        if valid_values:
+            imuMsg.orientation.x = float(quaternion_values[0])
+            imuMsg.orientation.y = float(quaternion_values[1])
+            imuMsg.orientation.z = float(quaternion_values[2])
+            imuMsg.orientation.w = float(quaternion_values[3])
+
+            imuMsg.angular_velocity.x = float(gyro_values[0])
+            imuMsg.angular_velocity.y = float(gyro_values[1])
+            imuMsg.angular_velocity.z = float(gyro_values[2])
+
+            imuMsg.linear_acceleration.x = float(linear_acceleration_values[0])
+            imuMsg.linear_acceleration.y = float(linear_acceleration_values[1])
+            imuMsg.linear_acceleration.z = float(linear_acceleration_values[2])
+
+            self.imu_output.publish(imuMsg)
+
+        else:
+            self.get_logger().debug(f"ERROR: One or more values was None.")
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -55,6 +96,7 @@ def main(args=None):
     rclpy.spin(imu_driver)
     imu_driver.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
