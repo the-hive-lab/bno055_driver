2430c551d7e7cce4910751ec1ce0e2515ba41582
Patrick Marshall <marshallpt@vcu.edu>
2022-02-09 16:33:21 -0500

    Updated frame_id
    updated frame_id to be imu_link, which is what is necessary
    for an imu.
    
    bno055_driver.py
    * updated frame_id in the header

--- bno055_driver/bno055_driver.py
+++ bno055_driver/bno055_driver.py
@@ -53,7 +53,7 @@ class ReadIMU(Node):
         """
         imuMsg = Imu()
         imuMsg.header.stamp = self.get_clock().now().to_msg()
-        imuMsg.header.frame_id = 'base_link'
+        imuMsg.header.frame_id = 'imu_link'
 
         valid_values = False
 
